16223b858fb6c5b31f11a39ec72a9f8f3921ca0c
jacobJalinskiBergen <jjalinski134544@bergen.edu>
2023-06-06 15:36:06 -0400

    For Nahee

---
+++ Day 2/missedStuff.py
@@ -0,0 +1,17 @@
+def empty():
+    pass
+
+def max(a, b):
+    return a if a >= b else b
+
+def main():
+    empty()
+
+    if True:
+        pass
+    
+    # "<true>" if "<conditional>" else "<false>"
+
+    print(max(5, 10))
+
+main()
